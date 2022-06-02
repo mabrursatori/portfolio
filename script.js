0fa32ac955ca0de613743df74972446510895bc5
mabrursatori <mabrursatori@gmail.com>
2022-06-02 16:49:06 +0700

    finish

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-button').addClass("show");
+        }else{
+            $('.scroll-up-button').removeClass("show");
+        }
+    });
+
+    // slide up script
+    $('.scroll-up-button').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    //typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Youtuber", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Youtuber", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // carousel script
